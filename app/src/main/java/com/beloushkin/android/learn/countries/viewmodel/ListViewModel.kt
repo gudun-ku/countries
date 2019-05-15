706b0e31b6270b8c2882b3f1d5ce2f0b5d3ea39c
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-05-15 22:17:23 +0400

    ViewModel first

--- app/src/main/java/com/beloushkin/android/learn/countries/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/beloushkin/android/learn/countries/viewmodel/ListViewModel.kt
@@ -1,6 +1,36 @@
 package com.beloushkin.android.learn.countries.viewmodel
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import com.beloushkin.android.learn.countries.model.Country
 
 class ListViewModel {
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(
+              Country("CountryA")
+            , Country("CountryB")
+            , Country("CountryC")
+            , Country("CountryD")
+            , Country("CountryE")
+            , Country("CountryF")
+            , Country("CountryG")
+            , Country("CountryH")
+            , Country("CountryI")
+            , Country("CountryJ")
+        )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
+
 }
